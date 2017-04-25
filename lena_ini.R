3a7706445bbf555543c2f72f9d81356952f17ff6
Yifan Zhu <zhuyifan@zhuyifandeair.student.iastate.edu>
2017-04-25 17:11:05 -0500

    delete old codes

--- lena_ini.R
+++
@@ -1,8 +0,0 @@
-z<-scan(file="~/Documents/isu/STAT 580/final_proj/lena256",skip=1)
-r_index<-seq(1,length(z),by=1)
-r_index<-sample(r_index)
-r_index_zero<-r_index[1:(length(z)*0.4)]
-z[r_index_zero]<-0
-z<-matrix(z,nrow=sqrt(length(z)),byrow=T)
-image(z)
-lambda<-seq(1,0.2,by=-0.2)
